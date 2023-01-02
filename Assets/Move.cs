73832eb5a80f814f9507088dc7b5b9dd50f25999
jadestar01 <104671580+jadestar01@users.noreply.github.com>
2023-01-02 15:17:05 +0900

    Add Move
    add move

---
+++ Assets/Move.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public int moveSpeed = 5;          //integer Á¤¼ö°ª
+    Rigidbody2D rigid;
+
+    private void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        rigid.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rigid.velocity.y);
+    }
+}
